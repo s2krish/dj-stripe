d5bef3ceb6b361144621693c2a1c170a29fd8f91
Krish <krishna.sunuwar@gmail.com>
2020-02-28 17:56:46 +0545

    external_accounts added

--- djstripe/migrations/0008_external_accounts.py
+++ djstripe/migrations/0008_external_accounts.py
@@ -10,7 +10,7 @@ import djstripe.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("djstripe", "0006_invoice_status"),
+        ("djstripe", "0007_update_invoice_subscription_models"),
     ]
 
     operations = [
